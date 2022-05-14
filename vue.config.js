0672b00fb08c7452552695dabef874304840be21
jimzandueta <jimzandueta@gmail.com>
2022-05-14 12:51:30 +0800

    Deplpoyment fixes

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/Accuweather-RCM/'
+  publicPath: '/accuw-rcm/'
 })
